1d04645de6ee1ef5d3483a17471fc3edafafcb55
Mathis Fituwi <mafi7060@colorado.edu>
2025-05-26 12:46:02 -0600

    posts test

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import { MenuLayout } from "./home";
 import { Footer } from "./home";
 import Image from 'next/image';
 import birdPic from '../assets/bird_with_balls.jpg';
+import posts from '../data/test_posts.json';
+
 
 export default function Home() {
   return (
@@ -14,6 +16,22 @@ export default function Home() {
           <h3 className="font-bold font-mono text-white">A bum. Negative to Zero sum. I'm going numb 🦬</h3>
           </div>
         </div>
+
+
+
+          {/* stuff*/}
+          <section className="mt-16 w-full flex justify-center">
+            <div className="bg-stone-600 p-6 rounded-lg w-full max-w-5xl text-white font-mono space-y-6">
+              <h2 className="text-4xl font-bold font-mono text-center">Stuff</h2>
+                {posts.map((post, i) => (
+                  <div key={i} className="border-l-4 border-white pl-5 bg-stone-700 p-6 rounded-lg w-full max-w-5xl text-white">
+                    <h3 className="text-2xl font-semibold">{post.title}</h3>
+                    <p className="text-sm text-gray-200">{post.date}</p>
+                    <p className="mt-2">{post.subtitle}</p>
+                  </div>
+                ))}
+            </div>
+          </section>
 
 
 
